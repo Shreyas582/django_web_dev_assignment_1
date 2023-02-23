a64f23ccc429341cf5f0db223a9273253c3d2142
Shreyas <shreyas.58@outlook.com>
2023-02-24 04:02:29 +0530

    For vercel deployment

--- newsapiproject/settings.py
+++ newsapiproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-urw&%(b7etc!^6m$48c6u2&j!4=g@#x=4(+b_a8n0r&7!b&0f$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
